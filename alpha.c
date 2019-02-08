fb3d32a119e3cad0ab8fdef3969ea01b9e404b6c
ajith-m128 <46275398+ajith-m128@users.noreply.github.com>
2019-02-08 22:28:20 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char i;
+    scanf("%c",&i);
+    if(i>='a' && i<='z')
+    {
+        printf("Alphabet");
+    }
+    else
+    printf("NO");
+    
+    return 0;
+}
