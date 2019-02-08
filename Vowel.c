e418b13d1346e54c85628f1e50b42f96843b799e
ajith-m128 <46275398+ajith-m128@users.noreply.github.com>
2019-02-08 22:19:00 +0530

    Create Vowel.c

---
+++ Vowel.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char b;
+    scanf("%c",&b);
+    if(b=='a'||b=='e'||b=='i'||b=='o'||b=='u'||b=='A'||b=='E'||b=='I'||b=='O'||b=='U')
+    {
+        printf("Vowels");
+    }
+    else if('b'>='a' && 'b'<='z')
+      }
+      printf("Consonent");
+      }
+      else
+      printf("invalid");
+
+    return 0;
+}
